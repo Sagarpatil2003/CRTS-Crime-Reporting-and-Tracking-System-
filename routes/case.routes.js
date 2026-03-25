18b86ed3a270b582475ba21c8bc0f20ef7b873ff
Sagarpatil2003 <sp4765699@gmal.com>
2026-03-25 17:19:10 +0530

    add releted more api routes

--- routes/case.routes.js
+++ routes/case.routes.js
@@ -1,36 +1,79 @@
 const express = require('express')
 const router = express.Router()
 const caseController = require('../controllers/case.controllers')
-const { createCaseSchema, updateStatusSchema } = require('../validators/case.validation')
-// middlewares
+const OfficerCaseController = require('../controllers/officer/case.controller')
+const { createCaseSchema, updateStatusSchema , nearbyCasesSchema } = require('../validators/case.validator')
+
+// Middlewares
 const auth = require('../middlewares/auth.middleware')
 const role = require('../middlewares/role.middleware')
 const permit = require('../middlewares/permission.middleware')
 const { validate } = require('../middlewares/validation.middleware')
-const workflow = require('../constants/caseWorkflow')
 const checkForDuplicateCase = require('../middlewares/duplicateCheck.middleware')
-const detectCluster = require('../middlewares/clusterDetection.middleware')
+const accessGuard = require('../middlewares/accessGuard')
 
 
-// All case routes require being logged in
-router.use(auth)
+/**
+ * @route   GET /cases/nearBycases
+ * @desc    Public news feed/map of nearby incidents
+ */
+router.get('/nearBycases', validate(nearbyCasesSchema), caseController.getNearbyCases)
 
+
+router.use(auth) 
+
+/**
+ * @route   GET /cases/my-cases
+ * @desc    Fetch list for the logged-in user (Moved up to avoid :id conflict)
+ */
+router.get('/my-cases', caseController.getMyCase)
+
+/**
+ * @route   POST /cases
+ * @desc    File a new report (Triggers CASE_REPORTED log)
+ */
 router.post('/',
-    role("CITIZEN", "OFFICER"),
+    role("CITIZEN","ADMIN"),
     permit("REPORT_CASE"),
     validate(createCaseSchema),
-    checkForDuplicateCase,
-    detectCluster,
+    checkForDuplicateCase, 
     caseController.createCase
 )
 
+/**
+ * @route   GET /cases/:id/logs
+ * @desc    Fetch the Audit Log Timeline
+ */
+router.get("/:id/logs",
+    role("CITIZEN", "OFFICER", "ADMIN", "LAWYER", "JUDGE"), 
+    permit("VIEW_CASE_LOGS"),
+    caseController.getCaseTimeline
+)
 
+/**
+ * @route   PATCH /cases/:id/status
+ * @desc    Strict workflow status change (Triggers STATUS_CHANGE log)
+ */
 router.patch("/:id/status",
     role("OFFICER", "JUDGE", "ADMIN"),
-    permit("INVESTIGATE_CASE", "FINALIZE_VERDICT"),
+    permit("UPDATE_CASE_STATUS"),
     validate(updateStatusSchema),
+    // validateWorkflow, 
     caseController.updateStatus
-);
+)
+
+/**
+ * @route   GET /cases/:id
+ * @desc    Specific details
+ */
+router.get("/:id", 
+    accessGuard, 
+    caseController.getCaseDetails
+)
+
+// ACCEPT THE CASE
+
 
 
 module.exports = router
+
