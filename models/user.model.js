d4be5bb653412cee54cf0cc2dd722f6dc3b3ca8e
Sagarpatil2003 <sp4765699@gmal.com>
2026-03-25 17:00:54 +0530

    add dbug pre save hook

--- models/user.model.js
+++ models/user.model.js
@@ -20,27 +20,20 @@ let UserSchema = new mongoose.Schema({
     password: { type: String, required: true, select: false },
     location: {
         type: { type: String, enum: ['Point'], default: 'Point' },
-        coordinates: { type: [Number], default: [0, 0] }
-    },
-
-    currentLocation: {
-        type: {
-            type: String,
-            enum: ["Point"],
-            default: "Point"
-        },
-        coordinates: {
-            type: [Number],
-            default: [0, 0]
+        coordinates: { type: [Number], default: [0, 0] },
+        address: {
+            type: String,   
+            trim: true
         }
     },
+
 
     availabilityStatus: {
         type: String,
         enum: ["AVAILABLE", "ON_CASE", "OFF_DUTY"],
         default: "AVAILABLE"
     },
-    badgeNumber: { type: String, sparse: true },
+    badgeNumber: { type: String, sparse: true, unique: true },
     isEmailVerified: { type: Boolean, default: false },
     accountStatus: {
         type: String,
@@ -57,17 +50,33 @@ let UserSchema = new mongoose.Schema({
 }, { timestamps: true })
 
 // passwords will be hashed
-UserSchema.pre('save', async function () {
-    if (!this.isModified("password")) return
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-})
+
+UserSchema.pre('save', async function (next) {
+
+    if (this.isModified("password")) {
+        const passwordString = String(this.password);
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(passwordString, salt);
+    }
+
+
+    if (this.role === 'OFFICER' && !this.badgeNumber) {
+        // Generates OFF + timestamp (e.g., OFF-1710425)
+        this.badgeNumber = `OFF-${Date.now().toString().slice(-7)}`;
+    }
+
+    next
+});
+
 
 // Instance Method for cleaner Logic
 UserSchema.methods.comparePassword = async function (candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password)
 }
 
-UserSchema.index({ currentLocation: "2dsphere" }); 
+
+
+
+UserSchema.index({ currentLocation: "2dsphere" });
 UserSchema.index({ location: "2dsphere" });
 module.exports = mongoose.model('User', UserSchema)
