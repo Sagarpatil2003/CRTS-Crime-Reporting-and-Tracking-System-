ab5d8768e9ad369b4dbf6eee45fe1d29beb8fd37
Sagarpatil2003 <sp4765699@gmal.com>
2026-03-25 22:35:14 +0530

    add to cloude both MDB and RDB

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose')
 
 async function connectDB () {
     try {
-       await mongoose.connect('mongodb://127.0.0.1:27017/SCI')
+       await mongoose.connect(process.env.MONGODB_URL)
        console.log('DB connected..')
     }catch(error) {
         console.log('Error in DB config')
@@ -11,3 +11,4 @@ async function connectDB () {
 }
 
 module.exports = connectDB
+
--- config/redis.config.js
+++ config/redis.config.js
@@ -1,9 +1,11 @@
 const Redis = require("ioredis");
 
 const redisConfig = {
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || 6379,
-    maxRetriesPerRequest: null, // Critical for BullMQ
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: 'default', // Force "default" instead of "SCI"
+    password: 'br59cBZ9J36IUHhzjKkcJxtLKg5WCytd', // Use the clean password
+    maxRetriesPerRequest: null,
 }
 
 const redisConnection = new Redis(redisConfig);
